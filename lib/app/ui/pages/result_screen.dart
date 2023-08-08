26d10149cd37ffc4548104f12edcb891df60b0c7
Anthony Josué Jara Bustamante <omarbush81@gmail.com>
2023-08-08 15:00:51 -0500

    Implement ResultScreen with ResultArguments
    Update ResultScreen to receive and display data using ResultArguments. Show sum of numbers and provide a back button to return to the previous screen.

--- lib/app/ui/pages/result_screen.dart
+++ lib/app/ui/pages/result_screen.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
+import 'package:miniproject/app/data/models/result_arguments.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    final data = ModalRoute.of(context)!.settings.arguments as ResultArguments;
+
+    final int sum = data.sum;
+    final int minNumber = data.minNumber;
+    final int maxNumber = data.maxNumber;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Result Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('Sum of numbers between $minNumber and $maxNumber: $sum'),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: const Text('Back'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
